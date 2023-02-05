d87610f12bf286ddad5b6523a3360633313fb61e
vitaliy.sherst.06 <vitaliy.sherst.06@gmail.com>
2023-02-05 10:03:16 +0300

    Fix

--- plugins/vuex.js
+++
@@ -1,6 +0,0 @@
-import {defineNuxtPlugin} from "nuxt/app";
-import store from "@/store/index";
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(store);
-});
--- store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "vuex";
-import emojis from "@/store/modules/emojis";
-
-export default createStore({
-    modules: {
-        emojis
-    }
-});
